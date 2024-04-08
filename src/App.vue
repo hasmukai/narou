ef5ba53d51371d184d1900c4828d5871f75f6412
hasmukai <ffix86190@gmail.com>
2024-04-09 07:14:36 +0900

    clicker

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import Counter from './components/Counter.vue';
 </script>
 
 <template>
@@ -9,6 +10,9 @@ import TheWelcome from './components/TheWelcome.vue'
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
+    </div>
+    <div>
+      <Counter />
     </div>
   </header>
 
---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+const count = ref<number>(0);
+const countMessage = computed(() => `The count is: ${count.value}`);
+</script>
+
+<template>
+    <div>
+        <h2>{{ countMessage }}</h2>
+        <button @click="count++">Increment</button>
+        <button @click="count=0">Reset</button>
+    </div>
+</template>
+
+
+<style></style>
